2f785c90e3fdd852a0025c8c6ea22cd343d537ab
MrBig <457965950@qq.com>
2025-09-02 17:39:38 +0800

    提交一版

---
+++ 2-个人优势.js
@@ -0,0 +1,7 @@
+export default [
+	"3年 Web 全栈（偏前端）开发经验，外加3年的前后端分离的前端开发经验。熟悉 HTML、CSS 相关知识，能够独立完成静态页面的编写，高精度还原UI设计稿；",
+	"熟悉使用 JavaScript ，使用过 Vue、React、ElementUI、AntDesign、JQuery 等框架进行开发，对实现原理有一定程度了解，切换其它框架也可胜任；",
+	"热衷于代码的组织封装，相比‘只管完成本次开发任务’，更习惯抽取通用逻辑完成封装以备它处复用，以提升代码的可维护性;",
+	"了解使用过 Hibernate、SpringMVC 等框架，掌握 MySQL 数据库的基本使用，了解 MVC 开发思想;",
+	"使用 Git 工具协同开发，使用 Webpack 打包前端 Vue 工程；",
+]
